55cad644c54b18d1f85a12167933f131df182be3
kiko-g <kikojpgoncalves@gmail.com>
2022-10-28 16:18:54 +0300

    fix: groups styles

--- pages/components/GroupStandings.tsx
+++ pages/components/GroupStandings.tsx
@@ -6,17 +6,19 @@ const GroupStandings = ({}: Props) => {
   const groups = Array.from({ length: 8 }, (_, k) => k + 2)
 
   return (
-    <div className="grid grid-cols-2 gap-x-8 gap-y-4">
+    <div className="grid grid-cols-1 gap-x-0 gap-y-6 xl:grid-cols-2 xl:gap-x-6">
       {groups.map((group, index) => (
-        <iframe
-          width="100%"
-          height="400"
-          frameBorder="0"
-          scrolling="no"
-          key={`group-${index}`}
-          id={`sofa-standings-embed-146${group}-41897`}
-          src={`https://www.sofascore.com/tournament/146${group}/41897/standings/tables/embed`}
-        ></iframe>
+        <div className="h-[425px] w-full rounded bg-light px-4 py-2 dark:bg-light">
+          <iframe
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            scrolling="no"
+            key={`group-${index}`}
+            id={`sofa-standings-embed-146${group}-41897`}
+            src={`https://www.sofascore.com/tournament/146${group}/41897/standings/tables/embed`}
+          ></iframe>
+        </div>
       ))}
     </div>
   )
