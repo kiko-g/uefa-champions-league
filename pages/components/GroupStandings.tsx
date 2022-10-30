32cf2cecd759d16853929b4d4eb18a98f0d17a38
kiko-g <kikojpgoncalves@gmail.com>
2022-10-31 01:32:15 +0200

    fix: react keys in group iframes

--- pages/components/GroupStandings.tsx
+++ pages/components/GroupStandings.tsx
@@ -8,13 +8,12 @@ const GroupStandings = ({}: Props) => {
   return (
     <div className="grid grid-cols-1 gap-x-0 gap-y-6 xl:grid-cols-2 xl:gap-x-6">
       {groups.map((group, index) => (
-        <div className="h-[425px] w-full rounded bg-light px-4 py-2 dark:bg-light">
+        <div className="h-[425px] w-full rounded bg-light px-4 py-2 dark:bg-light" key={`group-${index}`}>
           <iframe
             width="100%"
             height="100%"
             frameBorder="0"
             scrolling="no"
-            key={`group-${index}`}
             id={`sofa-standings-embed-146${group}-41897`}
             src={`https://www.sofascore.com/tournament/146${group}/41897/standings/tables/embed`}
           ></iframe>
